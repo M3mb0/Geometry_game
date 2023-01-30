5a9066b0fe56ba418498451ebe03d3ade3da6c5c
M3mb0 <cristian.ungureanu.86@gmail.com>
2023-01-30 22:32:26 +0200

    Area method added

--- geometry.py
+++ geometry.py
@@ -58,3 +58,6 @@ class Rectangle:
     def __init__(self, lower_left, upper_right):
         self.lower_left = lower_left
         self.upper_right = upper_right
+
+    def area(self):
+        return (self.upper_right.x - self.lower_left.x) * (self.upper_right.y - self.lower_left.y)
--- main.py
+++ main.py
@@ -5,11 +5,14 @@ point1 = Point(randint(0, 9), randint(0, 9))
 point2 = Point(randint(10, 19), randint(10, 19))
 my_rectangle = Rectangle(point1, point2)
 
-print("Rectangle coordinates:",
-      my_rectangle.lower_left.x, ",",
-      my_rectangle.lower_left.y, "and",
-      my_rectangle.upper_right.x, ",",
-      my_rectangle.upper_right.y)
+# print("Rectangle coordinates:",
+#       my_rectangle.lower_left.x, ",",
+#       my_rectangle.lower_left.y, "and",
+#       my_rectangle.upper_right.x, ",",
+#       my_rectangle.upper_right.y)
+
+print(f"Rectangle coordinates are: lower left point{point1} \
+and upper right point{point2}")
 
 user_input_x = int(input("Guess X: "))
 user_input_y = int(input("Guess Y: "))
@@ -21,3 +24,5 @@ print("Your point was inside the rectangle:",
 
 print(f"Distance between the A{point1} and B{point2} is: ",
       my_rectangle.upper_right.calculate_distance_to(my_rectangle.lower_left))
+
+print(f"The area is: {my_rectangle.area()}")
